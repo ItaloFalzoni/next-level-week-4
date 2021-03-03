c946a04a9ed4c56bd0c2d31279a7ee2c1b242f26
Ítalo Falzoni <italo.falzoni@gmail.com>
2021-03-03 10:20:39 -0300

    alterado o ícone de navegação do menu lateral

--- src/components/LateralMenu.tsx
+++ src/components/LateralMenu.tsx
@@ -12,17 +12,34 @@ export function LateralMenu() {
       <div>
         <img src="/logo.svg" alt="Logo MoveItalo" />
       </div>
-      <div className={tabActive == 'home' ? styles.active : styles.notActive}>
-        <button onClick={goToHome}>
-          <img src="/icons/home-active.svg" alt="Home" />
-        </button>
-      </div>
 
-      <div className={tabActive == 'leaderboard' ? styles.active : styles.notActive}>
-        <button onClick={goToLeaderboard}>
-          <img src="/icons/award-active.svg" alt="Awards" />
-        </button>
-      </div>
+      { tabActive == 'home' ? (
+        <>
+          <div className={styles.active}>
+            <a onClick={goToHome}>
+              <img src="/icons/home-active.svg" alt="Home" />
+            </a>
+          </div>
+          <div className={styles.notActive}>
+            <a onClick={goToLeaderboard}>
+              <img src="/icons/award-no-active.svg" alt="Awards" />
+            </a>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.notActive}>
+            <a onClick={goToHome}>
+              <img src="/icons/home-no-active.svg" alt="Home" />
+            </a>
+          </div>
+          <div className={styles.active}>
+            <a onClick={goToLeaderboard}>
+              <img src="/icons/award-active.svg" alt="Awards" />
+            </a>
+          </div>
+        </>
+      )}
 
     </div>
   )
