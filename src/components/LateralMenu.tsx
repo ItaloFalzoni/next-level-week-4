3d08a9fadf78cb31d611b93622bf8fb458db4e72
Ítalo Falzoni <italo.falzonI@gmail.com>
2021-03-08 17:09:39 -0300

    botão de logout adicionado ao componente LateralMenu

--- src/components/LateralMenu.tsx
+++ src/components/LateralMenu.tsx
@@ -1,45 +1,50 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 
+import { AuthenticationContext } from '../contexts/AuthenticationContext'
 import { NavigationContext } from '../contexts/NavigationContext'
 
 import styles from '../styles/components/LateralMenu.module.css'
 
 export function LateralMenu() {
   const { tabActive, goToHome, goToLeaderboard } = useContext(NavigationContext)
+  const { handleLogout } = useContext(AuthenticationContext)
 
   return (
     <div className={styles.container}>
-      <div>
-        <img src="/logo.svg" alt="Logo MoveItalo" />
-      </div>
+      <img src="/logo.svg" alt="Logo MoveItalo" />
 
-      { tabActive == 'home' ? (
-        <>
-          <div className={styles.active}>
-            <a onClick={goToHome}>
-              <img src="/icons/home-active.svg" alt="Home" />
-            </a>
-          </div>
-          <div className={styles.notActive}>
-            <a onClick={goToLeaderboard}>
-              <img src="/icons/award-no-active.svg" alt="Awards" />
-            </a>
-          </div>
-        </>
-      ) : tabActive == 'leaderboard' && (
-        <>
-          <div className={styles.notActive}>
-            <a onClick={goToHome}>
-              <img src="/icons/home-no-active.svg" alt="Home" />
-            </a>
-          </div>
-          <div className={styles.active}>
-            <a onClick={goToLeaderboard}>
-              <img src="/icons/award-active.svg" alt="Awards" />
-            </a>
-          </div>
-        </>
-      )}
+      <div className={styles.navigationMenu}>
+        {tabActive == 'home' ? (
+          <>
+            <div className={styles.active}>
+              <a onClick={goToHome}>
+                <img src="/icons/home-active.svg" alt="Home" />
+              </a>
+            </div>
+            <div className={styles.notActive}>
+              <a onClick={goToLeaderboard}>
+                <img src="/icons/award-no-active.svg" alt="Awards" />
+              </a>
+            </div>
+          </>
+        ) : tabActive == 'leaderboard' && (
+          <>
+            <div className={styles.notActive}>
+              <a onClick={goToHome}>
+                <img src="/icons/home-no-active.svg" alt="Home" />
+              </a>
+            </div>
+            <div className={styles.active}>
+              <a onClick={goToLeaderboard}>
+                <img src="/icons/award-active.svg" alt="Awards" />
+              </a>
+            </div>
+          </>
+        )}
+      </div>
+      <a onClick={handleLogout}>
+        <img src="/icons/logout.svg" alt="Logout" />
+      </a>
 
     </div>
   )
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,14 +6,9 @@ import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
   const { level } = useContext(ChallengesContext)
-  const { handleLogout } = useContext(AuthenticationContext)
 
   return (
     <div className={styles.profileContainer}>
-      <a className={styles.logout} onClick={handleLogout}>
-        <span>Logout</span>
-      </a>
-
       <img src="https://github.com/italofalzoni.png" alt="Ítalo Falzoni" />
       <div>
         <strong>Ítalo Falzoni</strong>
